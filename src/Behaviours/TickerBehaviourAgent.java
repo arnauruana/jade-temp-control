fa93ace32a9d0762941cc77885ae151e67290c5c
arnauruana <arnau.ruana.i@est.fib.upc.edu>
2020-03-10 19:19:21 +0100

    First approach to thermometer agent

--- src/Behaviours/TickerBehaviourAgent.java
+++ src/Behaviours/TickerBehaviourAgent.java
@@ -1,47 +1,46 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Behaviours;
+
 
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 
+import java.util.Random;
+
+
 /**
- * @author igomez
+ * @author Arnau Ruana | @arnauruana
  */
-public class TickerBehaviourAgent extends Agent {
-    public class HelloWorldTickerBehaviour extends TickerBehaviour {
-        String message;
-        int count_chocula;
-
-        public HelloWorldTickerBehaviour(Agent a, long period) {
-            super(a, period);
+public class TickerBehaviourAgent extends Agent
+{
+    public class Thermometer extends TickerBehaviour
+    {
+        public Thermometer(Agent agent, int period)
+        {
+            super(agent, period);
         }
 
-        public void onStart() {
-            this.message = "Agent " + myAgent + " with HelloWorldTickerBehaviour in action!!";
-            count_chocula = 0;
+        public void onStart()
+        {
+            System.out.println("Initialized agent: thermometer");
         }
 
-        public int onEnd() {
-            System.out.println("I have done " + count_chocula + " iterations");
-            return count_chocula;
+        public void onTick()
+        {
+            Random rand = new Random();
+            int temp = rand.nextInt(41) - 10;
+            System.out.println("Send random temperature: " + temp);
         }
 
-        public void onTick() {
-            System.out.println(this.message + count_chocula);
-            ++count_chocula;
+        public int onEnd()
+        {
+            System.out.println("Finalized agent: thermometer");
+            return 0;
         }
-
     }
 
-    protected void setup() {
-
-        HelloWorldTickerBehaviour b = new HelloWorldTickerBehaviour(this, 3000);
-        this.addBehaviour(b);
+    protected void setup()
+    {
+        Thermometer t = new Thermometer(this, 500);
+        this.addBehaviour(t);
     }
-
-
 }
